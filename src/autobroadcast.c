8667822d10638ca5b9def151d3adab7bd718fb64
Martin Skinner <martin.skinner@artcom.de>
2012-12-24 12:21:00 +0000

    we now automatically scan for valid broadcast addresses

--- src/autobroadcast.c
+++ src/autobroadcast.c
@@ -1,19 +1,51 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <netdb.h>
 #include "autobroadcast.h"
 
-#define BUFLEN 512
-#define SRV_IP "192.168.90.62"
+#define SRV_IP "192.168.90.255"
 
 ssize_t broadcast(unsigned short port, const void *buf, size_t len) {
     struct sockaddr_in si_other;
+    struct ifaddrs *ifaddr, *ifa;
     int s, i, slen=sizeof(si_other);
-    
+    int  on = 1;
+    int family;
+    char host[NI_MAXHOST];
+
+    /* iterate over all interfaces */
+    if (getifaddrs(&ifaddr) == -1) {
+        return -1;
+    } 
+    /* Walk through linked list, maintaining head pointer so we can free list later */
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+        if ((ifa->ifa_flags & IFF_BROADCAST) == 0)
+            continue;
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            s = getnameinfo(ifa->ifa_ifu.ifu_broadaddr, sizeof(struct sockaddr_in),
+                    host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+            if (s != 0) {
+                printf("getnameinfo() failed: %s\n", gai_strerror(s));
+                exit(EXIT_FAILURE);
+            }
+            printf("\taddress: <%s>\n", host);
+        }
+    }
+
+    freeifaddrs(ifaddr);  
+
+    /* send UDP DGRAM */ 
     if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
         return -1;
     }
-    
+    setsockopt(s, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
+
     memset((char *) &si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
     si_other.sin_port = htons(port);
--- tests/check_autobroadcast.c
+++ tests/check_autobroadcast.c
@@ -5,7 +5,8 @@
 #include <check.h>
 #include "../src/autobroadcast.h"
 
-#define SMALL_BUFFER 512
+#define SMALL_BUFFER_SIZE 512
+#define RECEIVE_BUFFER_SIZE 65536
 #define PORT 2345
 
 void
@@ -23,7 +24,7 @@ START_TEST (test_small_payload)
     /* set up listenling socket */
     struct sockaddr_in si_me, si_other;
     int s, i, bytes, slen=sizeof(si_other);
-    char recv_buf[SMALL_BUFFER], send_buf[SMALL_BUFFER];
+    char recv_buf[RECEIVE_BUFFER_SIZE], send_buf[SMALL_BUFFER_SIZE];
     
     fail_if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1, "listening socket");
     
@@ -34,17 +35,17 @@ START_TEST (test_small_payload)
     fail_if (bind(s, (struct sockaddr *) &si_me, sizeof(si_me)) == -1,"bind");
 
     /* send random data */
-    for(i = 0; i < SMALL_BUFFER; i++) {
+    for(i = 0; i < SMALL_BUFFER_SIZE; i++) {
       send_buf[i] = rand() % 255;
     }
-    broadcast(PORT, send_buf, SMALL_BUFFER); 
+    broadcast(PORT, send_buf, SMALL_BUFFER_SIZE); 
 
     /* check if data was received */
-    fail_if ((bytes = recvfrom(s, recv_buf, SMALL_BUFFER, MSG_DONTWAIT, (struct sockaddr *) &si_other, &slen))==-1, "receiving data");
+    fail_if ((bytes = recvfrom(s, recv_buf, RECEIVE_BUFFER_SIZE, MSG_DONTWAIT, (struct sockaddr *) &si_other, &slen))==-1, "receiving data");
     close(s);
     printf("received %d bytes\n", bytes);
-    fail_unless(bytes == SMALL_BUFFER, "received data length mismatch");
-    fail_unless(strncmp(send_buf, recv_buf, SMALL_BUFFER) == 0, "data not identical");
+    fail_unless(bytes == SMALL_BUFFER_SIZE, "received data length mismatch");
+    fail_unless(strncmp(send_buf, recv_buf, SMALL_BUFFER_SIZE) == 0, "data not identical");
     
     return;
 }
