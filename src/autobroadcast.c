083a21f1d0d3b82765fe2660774bd47725d0ef24
Martin Skinner <martin.skinner@artcom.de>
2012-12-24 15:44:43 +0000

    now uses automatically detected broadcasts addresses

--- src/autobroadcast.c
+++ src/autobroadcast.c
@@ -4,9 +4,8 @@
 #include <net/if.h>
 #include <ifaddrs.h>
 #include <netdb.h>
+#include <string.h>
 #include "autobroadcast.h"
-
-#define SRV_IP "192.168.90.255"
 
 ssize_t broadcast(unsigned short port, const void *buf, size_t len) {
     struct sockaddr_in si_other;
@@ -15,6 +14,12 @@ ssize_t broadcast(unsigned short port, const void *buf, size_t len) {
     int  on = 1;
     int family;
     char host[NI_MAXHOST];
+
+    /* send UDP DGRAM */ 
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        return -1;
+    }
+    setsockopt(s, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
 
     /* iterate over all interfaces */
     if (getifaddrs(&ifaddr) == -1) {
@@ -28,35 +33,17 @@ ssize_t broadcast(unsigned short port, const void *buf, size_t len) {
         if ((ifa->ifa_flags & IFF_BROADCAST) == 0)
             continue;
         if (ifa->ifa_addr->sa_family == AF_INET) {
-            s = getnameinfo(ifa->ifa_ifu.ifu_broadaddr, sizeof(struct sockaddr_in),
-                    host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
-            if (s != 0) {
-                printf("getnameinfo() failed: %s\n", gai_strerror(s));
-                exit(EXIT_FAILURE);
+            memcpy(&si_other, ifa->ifa_addr, sizeof(si_other));
+            si_other.sin_port = htons(port);
+
+            if (sendto(s, buf, len, 0, (struct sockaddr *) &si_other, slen)==-1) {
+                return -1;
             }
-            printf("\taddress: <%s>\n", host);
         }
     }
 
     freeifaddrs(ifaddr);  
 
-    /* send UDP DGRAM */ 
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
-        return -1;
-    }
-    setsockopt(s, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
-
-    memset((char *) &si_other, 0, sizeof(si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(port);
-    if (inet_aton(SRV_IP, &si_other.sin_addr)==0) {
-      return -1;
-    }
-    
-    if (sendto(s, buf, len, 0, &si_other, slen)==-1) {
-        return -1;
-    }
-    
     close(s);
     return 0;
 }
